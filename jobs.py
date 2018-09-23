c782499fd5f92d0826e0dca925cc787e0a85793c
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-09-23 12:04:00 +0200

    jobs: use self where appropriate

--- jobs.py
+++ jobs.py
@@ -20,55 +20,57 @@ class JobResult(Enum):
 
 class Job(object):
     id = 0
-    def __init__(s, name, cmd, env=None, hook=None, arg=None):
-        s.lock = Lock()
-        s.id = Job.id
+    def __init__(self, name, cmd, env=None, hook=None, arg=None):
+        self.lock = Lock()
+        self.id = Job.id
         Job.id += 1
 
-        s.name = name
-        s.cmd = cmd
-        s.env = env
-        s.worker = None
+        self.name = name
+        self.cmd = cmd
+        self.env = env
+        self.worker = None
 
-        s.hook = hook
-        s.arg = arg
+        self.hook = hook
+        self.arg = arg
 
-        s.result = JobResult.unknown
+        self.result = JobResult.unknown
 
-        s.time_created = -1
-        s.time_queued = -1
-        s.time_started = -1
-        s.time_finished = -1
+        self.time_created = -1
+        self.time_queued = -1
+        self.time_started = -1
+        self.time_finished = -1
 
-        s.set_state(JobState.created)
+        self.set_state(JobState.created)
 
-    def data_dir(s):
-        return s.name #,os.path.join(config.data_dir, s.name + "." + str(s.id))
+    def data_dir(self):
+        return self.name #,os.path.join(config.data_dir, s.name + "." + str(s.id))
 
-    def set_state(s, state, result=JobResult.unknown):
-        with s.lock:
-            s.state = state
-            s.result = result
-            if s.state == JobState.created:
-                s.time_created = time.time()
-            elif s.state == JobState.queued:
-                s.time_queued = time.time()
-            elif s.state == JobState.running:
-                s.time_started = time.time()
-            elif s.state == JobState.finished:
-                s.time_finished = time.time()
+    def set_state(self, state, result=JobResult.unknown):
+        with self.lock:
+            self.state = state
+            self.result = result
+            if self.state == JobState.created:
+                self.time_created = time.time()
+            elif self.state == JobState.queued:
+                self.time_queued = time.time()
+            elif self.state == JobState.running:
+                self.time_started = time.time()
+            elif self.state == JobState.finished:
+                self.time_finished = time.time()
 
-            log.info("Job %s: new state: %s %s %s %s %s %s " , s.name, s.state, s.result, s.time_created, s.time_queued, s.time_started, s.time_finished)
+            log.info("Job %s: new state: %s %s %s %s %s %s " ,
+                     self.name, self.state, self.result, self.time_created,
+                     self.time_queued, self.time_started, self.time_finished)
 
-        if s.hook:
-            s.hook(s.arg, s)
+        if self.hook:
+            self.hook(self.arg, self)
 
-    def stopped(s, result):
-        with s.lock:
-            s.state = result
+    def stopped(self, result):
+        with self.lock:
+            self.state = result
 
-    def cancel(s):
-        if s.worker:
-            s.worker.cancel(s)
+    def cancel(self):
+        if self.worker:
+            self.worker.cancel(self)
         else:
-            s.set_state(JobState.finished, JobResult.canceled)
+            self.set_state(JobState.finished, JobResult.canceled)
